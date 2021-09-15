4c5ab4c822db08fc3f159f411838eeb93ca1f666
Steven Mather <stevenrmather@gmail.com>
2021-09-15 16:52:44 -0700

    adding Reverser

---
+++ Warm-up-loops/Reverser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warm_up_loops
+{
+    public class Reverser
+    {
+        //string toReverse = "hello";
+        public string Reverse(string toReverse)
+        {
+            char[] charArray = toReverse.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray); 
+        }
+    }
+}
---
+++ Warm-up-loops_tests/Reverse-Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warm_up_loops;
+
+namespace Warm_up_loops_tests
+{
+    [TestClass]
+    public class Reverse_Tests
+    {
+        [TestMethod]
+        public void InputEven()
+        {
+            string inPut = "to";
+            String expected = "ot";
+            string outPut = "";
+            var thing = new Reverser();
+
+            outPut = thing.Reverse(inPut);
+
+            Assert.AreEqual(expected, outPut);
+        }
+        [TestMethod]
+        public void InputOdd()
+        {
+            string inPut = "hello";
+            String expected = "olleh";
+            string outPut = "";
+            var thing = new Reverser();
+
+            outPut = thing.Reverse(inPut);
+
+            Assert.AreEqual(expected, outPut);
+        }
+        
+    }
+}
